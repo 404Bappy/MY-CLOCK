fcfbd691ede20c84b782c30d36b69bfcb6b4fa50
Bappy71 <19103076@iubat.edu>
2022-10-21 23:17:47 +0600

    first commit

--- Clock.js
+++ Clock.js
@@ -2,3 +2,12 @@ const hourEl = document.getElementById('hour')
 const minutesEl = document.getElementById('minutes')
 const secondsEl = document.getElementById('seconds')
 const ampmEl = document.getElementById('ampm');
+
+
+function updareClock(){
+    let h  = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = 'AM'
+
+}
